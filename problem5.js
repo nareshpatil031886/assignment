3f28053b8101618d2fc5d9dec7a0fb0f3dfc789c
nareshpatil031886 <narendrapatil9247@gmail.com>
2025-04-12 11:30:43 +0530

    minor changes

--- problem5.js
+++
@@ -1,8 +0,0 @@
-function countMultiples(arr) {
-  const result = {};
-  for (let i = 1; i <= 9; i++) {
-    result[i] = arr.filter(num => num % i === 0).length;
-  }
-  console.log(result);
-}
-countMultiples([1,2,8,9,12,46,76,82,15,20,30]);
